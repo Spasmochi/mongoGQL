305bc042f26160d9e1df885b3cacb0a7c5688011
Spasmochi <spasmochi@gmail.com>
2022-04-09 18:16:23 +1000

    add lessons and mutation

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,8 +1,11 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { LessonService } from './lesson.service';
 import { LessonType } from './lesson.type';
 
 @Resolver((of) => LessonType)
 export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
+
   @Query((returns) => LessonType)
   Lesson() {
     return {
@@ -11,5 +14,14 @@ export class LessonResolver {
       startDate: '2020-01-01',
       endDate: '2020-01-02',
     };
+  }
+
+  @Mutation((returns) => LessonType)
+  createLesson(
+    @Args('name') name: string,
+    @Args('startDate') startDate: string,
+    @Args('endDate') endDate: string,
+  ) {
+    return this.lessonService.createLesson(name, startDate, endDate);
   }
 }
